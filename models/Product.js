57a0d34bb1e45e443126bf9e3ad2cf438ea989ad
faithelizagreen <faithgreen@knights.ucf.edu>
2021-10-28 15:55:25 -0400

    created associations using sequelize.

--- models/Product.js
+++ models/Product.js
@@ -39,7 +39,7 @@ Product.init({
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        mpodel: 'category',
+        model: 'category',
         key: 'id',
         unique: false
       }
--- models/index.js
+++ models/index.js
@@ -3,14 +3,33 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedProductTags = require('../seeds/product-tag-seeds');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany( Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through : {
+    model: ProductTag,
+    unique: false
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  } 
+})
 
 module.exports = {
   Product,
