19c8541f92f93c2a9533623e0b86aba2a251a4e6
faithelizagreen <faithgreen@knights.ucf.edu>
2021-10-29 20:07:45 -0400

    fixed route issues.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,6 +53,8 @@ router.put('/:id', async (req, res) => {
     if(!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
+    } else {
+      res.status(200).json(categoryData);
     }
   } catch (err) {
     res.status(500).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,7 +38,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create({
-      tag_id: req.body.tag_id,
+      tag_name: req.body.tag_name,
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -50,12 +50,14 @@ router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
-    where: {id : req.params.id }
-  });
-  if(!tagData) {
-    res.status(400).json({message: 'No tag found with this id!'});
-    return;
-  }
+      where: {id : req.params.id }
+    });
+    if(!tagData) {
+      res.status(400).json({message: 'No tag found with this id!'});
+      return;
+    } else {
+      res.status(200).json(tagData);
+    }
 } catch (err) {
   res.status(500).json(err);
 }
